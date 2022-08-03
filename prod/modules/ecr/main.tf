da5633357d8703d6afcd0d96ccb7f8eeb7740fb0
HelloMinchan <wjdalscksdle@naver.com>
2022-08-04 00:32:11 +0900

    fooiy.com ecr 생성

--- prod/modules/ecr/main.tf
+++ prod/modules/ecr/main.tf
@@ -1,9 +1,14 @@
 module "dev-api_fooiy_com" {
-  source = "./modules"
+  source              = "./modules"
   ecr_repository_name = "dev-api.fooiy.com"
 }
 
 module "api_fooiy_com" {
-  source = "./modules"
+  source              = "./modules"
   ecr_repository_name = "api.fooiy.com"
+}
+
+module "fooiy_com" {
+  source              = "./modules"
+  ecr_repository_name = "fooiy.com"
 }

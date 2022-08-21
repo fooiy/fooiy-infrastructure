c90c85b87b73dd0d0b1bdb31287460a5da642d23
HelloMinchan <wjdalscksdle@naver.com>
2022-08-21 17:35:03 +0900

    [FOOIY-519] SES DKIM 설정

--- prod/modules/ses/main.tf
+++ prod/modules/ses/main.tf
@@ -7,10 +7,44 @@ resource "aws_ses_domain_identity" "fooiy" {
   domain = "fooiy.com"
 }
 
+resource "aws_ses_domain_dkim" "fooiy" {
+  domain = aws_ses_domain_identity.fooiy.domain
+}
+
+resource "aws_route53_record" "fooiy_amazonses_dkim_record" {
+  count   = 3
+  zone_id = data.aws_route53_zone.route53.zone_id
+  name    = "${element(aws_ses_domain_dkim.fooiy.dkim_tokens, count.index)}._domainkey"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.fooiy.dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
+
 resource "aws_route53_record" "fooiy_amazonses_verification_record" {
   zone_id = data.aws_route53_zone.route53.zone_id
   name    = "_amazonses.fooiy.com"
   type    = "TXT"
   ttl     = "600"
   records = [aws_ses_domain_identity.fooiy.verification_token]
+}
+
+resource "aws_ses_domain_mail_from" "fooiy" {
+  domain           = aws_ses_domain_identity.fooiy.domain
+  mail_from_domain = "bounce.${aws_ses_domain_identity.fooiy.domain}"
+}
+
+resource "aws_route53_record" "fooiy_ses_domain_mail_from_mx" {
+  zone_id = data.aws_route53_zone.route53.zone_id
+  name    = aws_ses_domain_mail_from.fooiy.mail_from_domain
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 feedback-smtp.ap-northeast-2.amazonses.com"]
+}
+
+resource "aws_route53_record" "fooiy_ses_domain_mail_from_txt" {
+  zone_id = data.aws_route53_zone.route53.zone_id
+  name    = aws_ses_domain_mail_from.fooiy.mail_from_domain
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com -all"]
 }
